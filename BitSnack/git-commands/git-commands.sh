8df02a0d7c3944bd1047aa379682021465783f92
Fili Santillán <fili06604@gmail.com>
2020-08-04 07:56:44 -0500

    🙈 Removed the .sh extension from the gitignore

---
+++ BitSnack/git-commands/git-commands.sh
@@ -0,0 +1,22 @@
+git config --global user.name
+git config --global user.email
+
+git init
+git clone
+
+git status
+git add
+
+git commit
+git log
+
+git diff
+
+git branch
+git checkout
+
+git merge
+git reset 
+
+git pull
+git push
